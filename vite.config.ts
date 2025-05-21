29c0bc31484200d3b5ce52f6a00bada6307c9886
matheussanchesgarcia <sanchesmatheus88@gmail.com>
2025-05-21 08:03:45 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/relatorio-sem-parar/", // 👈 Adicione esta linha
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
